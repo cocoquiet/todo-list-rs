a32e5688d02ec5f84a5856f9ccd010430555ea82
JiSeong Lee <cocoquiet@knu.ac.kr>
2025-02-27 08:24:59 +0900

    Feat: Add persistence

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 use eframe::egui;
+use serde::{Deserialize, Serialize};
+use std::fs;
+
+const FILE_PATH: &str = "todo_list.json";
 
 fn main() {
     let options = eframe::NativeOptions::default();
     if let Err(e) = eframe::run_native(
         "TODO List",
         options,
-        Box::new(|_cc| Box::new(TodoApp::default())),
+        Box::new(|_cc| Box::new(TodoApp::load_todos())),
     ) {
         eprintln!("Error: {}", e);
     }
 }
 
+#[derive(Serialize, Deserialize)]
 struct TodoApp {
     todos: Vec<String>,
     new_todo: String,
@@ -21,6 +26,23 @@ impl Default for TodoApp {
         Self {
             todos: vec![],
             new_todo: Default::default(),
+        }
+    }
+}
+
+impl TodoApp {
+    fn load_todos() -> Self {
+        if let Ok(data) = fs::read_to_string(FILE_PATH) {
+            if let Ok(app) = serde_json::from_str(&data) {
+                return app;
+            }
+        }
+        Self::default()
+    }
+
+    fn save_todos(&self) {
+        if let Ok(json) = serde_json::to_string_pretty(self) {
+            let _ = fs::write(FILE_PATH, json);
         }
     }
 }
@@ -36,6 +58,7 @@ impl eframe::App for TodoApp {
                     if !self.new_todo.is_empty() {
                         self.todos.push(self.new_todo.clone());
                         self.new_todo.clear();
+                        self.save_todos();
                     }
                 }
             });
@@ -51,10 +74,13 @@ impl eframe::App for TodoApp {
                     }
                 });
             }
-            for i in remove_indices.iter().rev() {
-                self.todos.remove(*i);
+
+            if !remove_indices.is_empty() {
+                for i in remove_indices.iter().rev() {
+                    self.todos.remove(*i);
+                }
+                self.save_todos();
             }
         });
     }
-    
 }
